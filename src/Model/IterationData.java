c803f7a131d35d60c02dbe9a69af6644749a2741
haerambhan <haerambhan@gmail.com>
2020-10-09 09:37:01 +0530

    Added Analysis feature

---
+++ src/Model/IterationData.java
@@ -0,0 +1,11 @@
+package Model;
+
+public class IterationData{
+	public int loopCount;
+	public int maxLevel;
+	public boolean isValid;
+	@Override
+	public String toString() {
+		return "IterationData [loopCount=" + loopCount + ", maxLevel=" + maxLevel + "]";
+	}
+}

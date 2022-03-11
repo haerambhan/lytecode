300e720cee056723edfd3906348bc7ec8d3241f8
haerambhan <haerambhan@gmail.com>
2022-03-11 15:07:11 +0530

    Separate branch for a simple code editor

--- src/Model/NoRecordsFoundException.java
+++
@@ -1,9 +0,0 @@
-package Model;
-
-public class NoRecordsFoundException extends Exception {
-
-	private static final long serialVersionUID = 1L;
-	public NoRecordsFoundException(String message) {
-		super(message);
-	}
-}
